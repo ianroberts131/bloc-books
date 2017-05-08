60225e7f85e17d1efaa5523f315adcd00b30b8f0
ianroberts131 <ianroberts131@gmail.com>
2017-05-08 01:45:30 +0000

    Assignment 4 - almost complete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,9 +18,23 @@ class BooksController < BlocWorks::Controller
   end
   
   def create
+    puts "The params are: #{params.inspect}"
+    @book = Book.create(name: params["book"]["name"], author: params["book"]["author"], pages: params["book"]["pages"] )
+    if @book.save
+      redirect_to "books/#{@book.id}"
+    else
+      redirect_to "books/new"
+    end
   end
   
   def edit
+    @book = Book.find(params['id'])
+    
+    if @book.save
+      redirect_to("http://www.google.com")
+    else
+      redirect_to("http://www.yahoo.com")
+    end
   end
   
   def update
